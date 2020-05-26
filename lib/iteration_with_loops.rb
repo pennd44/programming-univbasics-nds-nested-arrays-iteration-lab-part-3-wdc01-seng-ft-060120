c98fb4fe27d18e35dc985fb7284b14336110e3db
Penn M Diba <pdiba@oxbowschool.org>
2020-05-26 02:24:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,14 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  newstring = ""
+  row = 0 
+  while row < src.length do
+    column = 0
+    while column < src[row].length
+    
+      column += 1 
+    end
+    row += 1 
+  end
 end
